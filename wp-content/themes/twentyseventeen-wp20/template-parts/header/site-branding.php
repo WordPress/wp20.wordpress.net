8ca12bfc9bd339c06215ad8bb1a3a6806166d124
Adam Wood <adam.wood@automattic.com>
2023-03-03 08:43:45 +1300

    Style header

---
+++ wp-content/themes/twentyseventeen-wp20/template-parts/header/site-branding.php
@@ -0,0 +1,6 @@
+<div class="site-branding">
+	<?php the_custom_logo(); ?>
+	<div class="site-branding-text">
+		<em><? esc_html_e( 'On to the remaining 60% of the web.', 'twentyseventeen' ) ?></em>
+	</div><!-- .site-branding-text -->
+</div><!-- .site-branding -->
