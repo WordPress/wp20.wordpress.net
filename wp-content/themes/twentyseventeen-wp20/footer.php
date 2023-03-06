25d55dfec392cbb856eb3c0e8d9c3b22aa5ee78f
Adam Wood <adam.wood@automattic.com>
2023-03-06 16:00:06 +1300

    Style footer

--- wp-content/themes/twentyseventeen-wp20/footer.php
+++ wp-content/themes/twentyseventeen-wp20/footer.php
@@ -1,16 +1,38 @@
 			<div class="wrap">
+
+			<?php if ( has_nav_menu( 'social' ) ) :
+			?>
+				<div class="wp20-social">
+					<p>Follow WordPress for upcoming details</p>
+					<nav class="social-navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'social',
+								'menu_class'     => 'social-links-menu',
+								'depth'          => 1,
+								'link_before'    => '<span class="screen-reader-text">',
+								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+								)
+							);
+							?>
+					</nav><!-- .social-navigation -->
+				</div>
+				<?php
+			endif;
+			?>
+
 				<div class="wp20-confetti-divider">
 					<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/wp20-logo-blue.svg" alt="<?php esc_html_e( 'WP20 logo', 'wp20' ); ?>" itemprop="logo" />
 				</div>
 
 				<p class="wp10-nostalgia">
 					<?php printf(
-						wp_kses_data( __( 'Check out <a href="%s">this post about the WordPress 10th anniversary</a>.', 'wp20' ) ),
-						'https://wordpress.org/news/2013/05/ten-good-years/'
+						wp_kses_data( __( 'Feeling nostalgic? Check out <a href="%s">this post about the WordPress 15th anniversary</a>.', 'wp20' ) ),
+						'https://wordpress.org/news/2018/04/celebrate-the-wordpress-15th-anniversary-on-may-27/'
 					); ?>
 				</p>
-			</div>	
-
+			</div>
 		</div><!-- #content -->
 
 
@@ -18,24 +40,6 @@
 			<div class="wrap">
 				<?php
 				get_template_part( 'template-parts/footer/footer', 'widgets' );
-
-				if ( has_nav_menu( 'social' ) ) :
-					?>
-					<nav class="social-navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu(
-								array(
-									'theme_location' => 'social',
-									'menu_class'     => 'social-links-menu',
-									'depth'          => 1,
-									'link_before'    => '<span class="screen-reader-text">',
-									'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-								)
-							);
-						?>
-					</nav><!-- .social-navigation -->
-					<?php
-				endif;
 
 				get_template_part( 'template-parts/footer/site', 'info' );
 				?>
