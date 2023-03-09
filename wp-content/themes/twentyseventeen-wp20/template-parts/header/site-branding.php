d67cd7b6296d15cf5b37dd135a10aa751d6cc6b7
dufresnesteven <steve.dufresne@automattic.com>
2023-03-09 09:58:30 +0900

    Update the header to match designs.

--- wp-content/themes/twentyseventeen-wp20/template-parts/header/site-branding.php
+++ wp-content/themes/twentyseventeen-wp20/template-parts/header/site-branding.php
@@ -1,6 +1,9 @@
 <div class="site-branding">
 	<?php the_custom_logo(); ?>
+
+	<?php if ( is_front_page() ): ?>
 	<div class="site-branding-text">
 		<em><? esc_html_e( 'On to the remaining 60% of the web.', 'twentyseventeen' ) ?></em>
 	</div><!-- .site-branding-text -->
+	<?php endif; ?>
 </div><!-- .site-branding -->
