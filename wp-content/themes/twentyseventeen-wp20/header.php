9bc10a334612caa0c83fa83a06d4d305832ca446
dufresnesteven <steve.dufresne@automattic.com>
2023-03-14 11:50:29 +0900

    Get nav working for desktop as well.

--- wp-content/themes/twentyseventeen-wp20/header.php
+++ wp-content/themes/twentyseventeen-wp20/header.php
@@ -22,17 +22,11 @@ use WP15\Locales;
 			<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
 
 			<?php if ( has_nav_menu( 'top' ) ) : ?>
-				<div class="navigation-top">
-					<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
-						<?php
-						echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
-						echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
-						_e( 'Menu', 'twentyseventeen' );
-						?>
-					</button>
-					<?php Locales\locale_switcher(); ?>
-				</div>
-				<?php get_template_part( 'template-parts/header/navigation', 'top' ); ?>
+					<div class="navigation-top">
+						<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+						<?php Locales\locale_switcher(); ?>
+					</div>
+				
 				<div class="navigation-top-menu-container" style="width:100%;" data-placeholder="<?php esc_attr_e( 'Search languages...', 'wp20' ); ?>"></div>
 			<?php endif; ?>
 		</header>
