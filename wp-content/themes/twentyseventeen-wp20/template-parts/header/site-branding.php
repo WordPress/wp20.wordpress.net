a12ec7eb004b0fa1f05816462ea5c6732a8de9d3
Adam Wood <adam.wood@automattic.com>
2023-03-28 16:15:43 +1300

    Update header

--- wp-content/themes/twentyseventeen-wp20/template-parts/header/site-branding.php
+++ wp-content/themes/twentyseventeen-wp20/template-parts/header/site-branding.php
@@ -1,4 +1,9 @@
 <div class="site-branding">
+
+	<a class="wp20-wordpress-mark" href="https://wordpress.org/">
+		<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/wordpress-mark.svg" alt="<?php esc_html_e( 'WordPress mark', 'wp20' ); ?>">
+	</a>
+
 	<?php the_custom_logo(); ?>
 
 	<?php if ( is_page( 'whats-on' ) ): ?>
