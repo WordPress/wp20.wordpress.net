102919682a0fd6140e3214519ad9faf5f9a2a5a0
ren <18050944+renintw@users.noreply.github.com>
2023-04-03 12:51:22 +0800

    Fix env/setup.sh

--- env/setup.sh
+++ env/setup.sh
@@ -4,7 +4,7 @@ wp rewrite structure '/%postname%/'
 wp rewrite flush
 
 # Set the layout to One Column
-theme mod set page_layout one-column
+wp theme mod set page_layout one-column
 
 # Run all cron tasks (Including Handbook imports from GitHub)
 wp cron event run --all
