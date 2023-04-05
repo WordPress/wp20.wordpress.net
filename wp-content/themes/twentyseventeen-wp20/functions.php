68a0ddf9d1d48e900e828fd4620784975ac910d4
Adam Wood <adam.wood@automattic.com>
2023-04-05 12:49:34 +1200

    News updates

--- wp-content/themes/twentyseventeen-wp20/functions.php
+++ wp-content/themes/twentyseventeen-wp20/functions.php
@@ -323,6 +323,30 @@ function render_social_meta_tags() {
 }
 
 /**
+ * Gets a nicely formatted string for the published date.
+ */
+function posted_datetime() {
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf(
+		$time_string,
+		get_the_date( DATE_W3C ),
+		get_the_date(),
+		get_the_modified_date( DATE_W3C ),
+		get_the_modified_date()
+	);
+
+	return sprintf(
+		/* translators: %s: Post date. */
+		__( '<span class="screen-reader-text">Posted on</span> %s', 'twentyseventeen' ),
+		$time_string
+	);
+}
+
+/**
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function render_news_posted_on() {
@@ -333,5 +357,5 @@ function render_news_posted_on() {
 		'<span class="author vcard"><a href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author_meta( 'display_name' ) . '</a></span>'
 	);
 
-	echo '<span class="byline"> ' . $byline . '</span>·<span class="posted-on">' . twentyseventeen_time_link() . '</span>';
+	echo '<span class="byline"> ' . $byline . '</span>·<span class="posted-on">' . posted_datetime() . '</span>';
 }
