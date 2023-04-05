27cf080a8a130b399639e4921744a5064195b811
Adam Wood <adam.wood@automattic.com>
2023-04-05 14:10:48 +1200

    Use 'News' as page title in mobile nav when viewing a single post

--- wp-content/themes/twentyseventeen-wp20/template-parts/header/navigation-top.php
+++ wp-content/themes/twentyseventeen-wp20/template-parts/header/navigation-top.php
@@ -3,11 +3,13 @@
  * Displays top navigation
  */
 
+$title = is_single() ? __( 'News', 'wp20' ) : get_the_title();
+?>
 
-?>	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
+<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
 		<img class="icon-bars" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/menu-icon.svg" aria-hidden="true"  />
 		<img class="icon-close" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/close-icon.svg" aria-hidden="true" />
-		<span><?php esc_html_e( get_the_title() ); ?></span>
+		<span><?php esc_html_e( $title ); ?></span>
 </button>
 
 <nav id="site-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'wp20' ); ?>">
