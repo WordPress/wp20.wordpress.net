e812e3df04684d216e6cfcbd6c939afcdbfbb3a2
Adam Wood <adam.wood@automattic.com>
2023-04-27 15:02:23 +1200

    Removed unused .navigation-top-menu-container

--- wp-content/themes/twentyseventeen-wp20/header.php
+++ wp-content/themes/twentyseventeen-wp20/header.php
@@ -27,7 +27,6 @@ use WP20\Locales;
 						<div class="wrap wrap-wide">
 							<?php get_template_part( 'template-parts/header/navigation', 'top' ); ?>
 							<?php Locales\locale_switcher(); ?>
-							<div class="navigation-top-menu-container"></div>
 						</div>
 					</div>
 				</div>
